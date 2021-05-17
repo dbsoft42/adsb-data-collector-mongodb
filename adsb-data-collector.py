712661f4b2b5f125fa1209f616a99dd26ad670d5
Debarka Banik <debarka.banik@gmail.com>
2021-05-17 22:14:02 +0530

    Initial commit. First working version. Some parts are WIP. Bugs expected, of course.

---
+++ adsb-data-collector.py
@@ -0,0 +1,128 @@
+import asyncio
+import aiohttp
+from motor.motor_asyncio import AsyncIOMotorClient
+from datetime import datetime, time
+from dateutil.relativedelta import relativedelta
+from copy import deepcopy
+from config import config
+
+messages = {}
+
+async def cleanup():
+    '''
+    Trims the messages dict that grows over time
+    based on max age defined in config dict
+    '''
+    while True:
+        now = datetime.now()
+        cutoff_time = now - relativedelta(seconds=config['messages_max_age'])
+        for key, value in messages.items():
+            if value['time'] < cutoff_time:
+                del messages[key]
+        await asyncio.sleep(config['cleanup_run_interval'])
+
+async def process_dataset(db, dataset):
+    '''
+    Processes the dump1090 JSON dataset, process each record/message into messages dict
+    and call process_message(message) for each
+    '''
+    # Loop through each item of the aircraft list - we call them messages
+    for message in dataset['aircraft']:
+        aircraft = message['hex']
+        if aircraft not in messages:
+            # Add to the list as skeleton dict if not existing
+            messages[aircraft] = {'status':{}, 'first_flight_message':False, 'processed':False}
+        # Keep a copy of the old status for comparison
+        old_status = deepcopy(messages[aircraft]['status'])
+        # Fill/replace each attribute below
+        for key, value in message.items():
+            # Set flag if flight ID is received for the first time
+            if (key == 'flight' and len(value) > 0
+                and (
+                        'flight' not in messages[aircraft]['status']
+                        or len(messages[aircraft]['status']['flight']) == 0
+                    )
+                ):
+                messages[aircraft]['first_flight_message'] = True
+            messages[message['hex']]['status'][key] = value
+            # In future consider skipping and unsetting zero/empty values
+        # Set the processed flag and time if status is changed from earlier
+        if messages[aircraft]['status'] != old_status:
+            messages[aircraft]['processed'] = False
+            messages[aircraft]['time'] = datetime.fromtimestamp(dataset['now'])
+        asyncio.create_task(process_message(db, messages[aircraft]))
+        #await process_message(messages[aircraft])
+
+async def process_message(db, message):
+    '''Process given message into DB records as needed by calling required sub-functions'''
+    if not message['processed']:
+        status = message['status'].copy()
+        status['time'] = message['time']
+        # Insert aircraft into DB if not already existing or update 'last seen'
+        db_aircraft = await db.aircraft.find_one({'hex':status['hex']})
+        if  db_aircraft == None:
+            await db.aircraft.insert_one({  'hex':          status['hex'],
+                                            'first seen':   status['time'],
+                                            'last seen':    status['time']
+                                        })
+        else:
+            await db.aircraft.update_one({'hex':status['hex']}, {'$set':{'last seen':status['time']}})
+        # If flight available
+        if (
+            'flight' in status
+            and len(status['flight']) > 0
+            ):
+            # Insert flight into DB if not erxisting or update 'last seen'
+            db_flight = await db.flights.find_one({ 'flight':status['flight'], 'hex':status['hex']})
+            if  db_flight == None:
+                await db.flights.insert_one({   'flight':       status['flight'],
+                                                'hex':          status['hex'],
+                                                'first seen':   status['time'],
+                                                'last seen':    status['time'],
+                                                'seen on':      [datetime.combine(status['time'].date(), time.fromisoformat('00:00'))]
+                                            })
+            else:
+                await db.flights.update_one({'flight':status['flight'], 'hex':status['hex']},
+                                                {'$set':{
+                                                        'last seen':status['time']
+                                                        },
+                                                '$addToSet':{
+                                                        'seen on':datetime.combine(status['time'].date(), time.fromisoformat('00:00'))
+                                                        }
+                                                }
+                                            )
+        # Create status record
+        await db.status.insert_one(status)
+        # If flight ID received for the first time
+        if message['first_flight_message']:
+            # Update older status records (which don't have the flight ID yet)
+            db.status.update_many(  {
+                                        'hex':status['hex'],
+                                        'time':{'$gte':status['time']-relativedelta(seconds=config['orphan_status_update_max_age'])}
+                                    },
+                                    {'$set':{'flight':status['flight']}}
+                                )
+            # Add 'seen on' element to flight
+            db.flights.update_one({ 'flight':status['flight'], 'hex':status['hex']},
+                                    {'$addToSet':{'seen on':status['time']}})
+            messages[status['hex']]['first_flight_message'] = False
+        messages[status['hex']]['processed'] = True
+
+async def main():
+    '''The driver function - will get the JSON from the URL and call process_dataset'''
+    # Initialize DB connection
+    mdb = AsyncIOMotorClient(config['db']['mongodb_conn_str'])
+    db = mdb[config['db']['database_name']]
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=config['http_timeout'])) as http_session:
+        asyncio.create_task(cleanup())
+        while True:
+            try:
+                async with http_session.get(config['dump1090_url']) as response:
+                    dataset = await response.json()
+                    await process_dataset(db, dataset)
+            except:
+                print(f'EXCEPTION!!!')
+            await asyncio.sleep(config['source_poll_interval'])
+
+asyncio.run(main())
---
+++ config_template.py
@@ -0,0 +1,40 @@
+# Make a copy of this file as config.py and update the required values below.
+# The MongoDB connection details and the dump1090 URL are the ones you really need to look at.
+# The rest can be left at the defaults to start with.
+
+config = {} # Ignore this line
+
+# MongoDB connection details (don't miss the username and password):
+config['db'] = {
+                'mongodb_conn_str':
+                    'mongodb+srv://username:password@url.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+                'database_name': 'adsb'
+                } #Include username and password in mongodb_conn_str
+
+# Dump1090 URL for the aircraft.json file
+config['dump1090_url'] = 'http://localhost/dump1090/data/aircraft.json'
+
+# Once a flight ID is received for the first time,
+# status records already inserted to the DB can be updated with the flight ID.
+# Records only up to these many seconds will be updated.
+config['orphan_status_update_max_age'] = 600 # seconds
+
+# The source (dump1090 JSON URL) will be checked every:
+# This affects how often the script checks dump1090 for updated status
+# Increasing this interval is a way to reduce amount of data going into the DB.
+config['source_poll_interval'] = 1 # seconds
+
+# The timeout for the HTTP request to get the JSON file from dump1090
+# If a response if not received within this period, the request will be aborted.
+# This will not fail the script and it will try the next request after the source_poll_interval.
+# If your dump1090 is running locally, a short timeout is fine.
+# If your source_poll_interval is short, better to keep the timeout short.
+config['http_timeout'] = 3 # seconds
+
+# A dictionary is used to maintain a local of copy of the ADS-B status messages
+# so that new messages can be distinhuished from those already processed.
+# A cleanup function periodically trims this dictionary to keep it from growing too much.
+# The following 2 options control how often the cleanup runs
+config['cleanup_run_interval'] = 3600 # seconds
+# and how old the messages can be before they are removed.
+config['messages_max_age'] = 1800 # seconds
